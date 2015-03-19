27c299a179ef059c19cd6453b41c4c2ce100ce0a
Tim Mecklem <tim@teamgaslight.com>
2015-03-19 15:06:09 -0400

    Trying with csv post
    Signed-off-by: Tim Mecklem <tim@teamgaslight.com>

--- spec/controllers/streaming_controller_spec.rb
+++ spec/controllers/streaming_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe StreamingController, type: :controller do
 
-  describe "GET #stream" do
+  describe "POST #stream" do
     it "returns http success" do
-      get :stream, format: :csv
+      post :stream, file: fixture_file_upload('upload.csv'), format: :csv
       expect(response).to have_http_status(:success)
       expect(CSV.parse(response.body).size).to eq 100
     end
